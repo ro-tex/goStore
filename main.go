d76ad2ab0f52b0ca9b058cb301e6fcfd4d3f4d67
Ivaylo Novakov <inovakov@gmail.com>
2019-08-19 10:44:17 +0200

    Attempt at multilog.

--- main.go
+++ main.go
@@ -3,12 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
 
 	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
-
-	"goStore/handlers"
-	"goStore/middlewares"
 )
 
 /*
@@ -44,7 +41,44 @@ func logError(e *error) {
 	fmt.Println("[logError] Response: " + string(jsonE))
 }
 
+func multiLog(event interface{}) {
+	var err error
+	defer func() {
+		if err != nil {
+			fmt.Println("[Error][multiLog]:", err.Error())
+		}
+	}()
+
+	switch reflect.TypeOf(event).Name() {
+	case "*events.APIGatewayProxyRequest":
+		r := event.(*events.APIGatewayProxyRequest)
+		j, err := json.Marshal(r)
+		if err != nil {
+			return
+		}
+		fmt.Println("[Request] " + string(j))
+	case "*events.APIGatewayProxyResponse":
+		res := event.(*events.APIGatewayProxyResponse)
+		j, err := json.Marshal(res)
+		if err != nil {
+			return
+		}
+		fmt.Println("[Response] " + string(j))
+	case "*errors.errorString":
+		r := event.(*error)
+		j, err := json.Marshal(r)
+		if err != nil {
+			return
+		}
+		fmt.Println("[Error] " + string(j))
+	default:
+		fmt.Println("WARN: Trying to log unknown object of type " + reflect.TypeOf(event).Name())
+	}
+}
+
 func main() {
+	// TODO: check if API Gateway's error is errorstring or an actual *error
+
 	middlewares.RegisterRequestMW(middlewares.CleanRequest)
 	middlewares.RegisterResponseMW(middlewares.GnuTerryPratchett)
 	middlewares.RegisterResponseMW(logResponse)
